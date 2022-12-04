d5e2007e74541620fc8fd61748d54ba13405afbf
bielmorato <bielmorato09@gmail.com>
2022-12-04 17:04:54 -0300

    "alteração aut, e singup"

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,11 +1,11 @@
 
 import { useState, createContext, useEffect } from 'react';
-// import firebase from '../services/firebaseConnection';
+import axios from 'axios';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }){
-  const [user, setUser] = useState({id:1, nome: 'Gabriel'}); // -> set para forçar um usuario logado
+  const [user, setUser] = useState(null); // -> set para forçar um usuario logado
   const [loadingAuth, setLoadingAuth] = useState(false);
   const [loading, setLoading] = useState(true);
 
@@ -26,34 +26,43 @@ function AuthProvider({ children }){
 
   }, [])
 
-  /*async function signUp(email, password, nome){ 
-    função pra cadastrar o usuario
-    setLoadingAuth(true);
-    await firebase.auth().createUserWIthEmailAndPassowrd(email, password)
-    .then(async (value) =>{
-      let uid = value.user.uid;
-      await firebase.firestore().collection('users')
-      .doc(uid).set({
-        nome: nome,
-        avatarUrl: null,
-      })
-      .then(()=>{
-        let data = {
-          uid: uid,
-          nome: nome,
-          email: value.user.email,
-          avatarUrl: null
-        };
-
-        setUser(data);
-      })
+  async function signUp(email, password, nome, birthDate){
+    setLoading(true);
+    let config = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "*",
+      }
+  }
+    const baseUrl = 'http://localhost:8081/instructor';
+    axios.post(baseUrl, {
+      name: nome,
+      email: email,
+      password: password,
+      birthDate: birthDate
+    }, config
+    ).then((response) => {
+      setUser(response.data);
+      storageUser(response.data);
+      setLoadingAuth(false);
+      console.log('olá')
+      console.log(response.data)
     })
+    .catch((error)=>{
+      console.log(error);
+      setLoadingAuth(false);
+    })
+    
+    function storageUser(data){
+      localStorage.setItem('SistemaUser', JSON.stringify(data));
+    }
 
-  }*/
+  }
 
 
   return(
-    <AuthContext.Provider value={{ signed: !!user,  user, loading }}>
+    <AuthContext.Provider value={{ signed: !!user,  user, loading, signUp }}>
       {children}
     </AuthContext.Provider>
   )
