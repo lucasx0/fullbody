8bbbeac330552a375c0efaeef624700f7e45dbae
lucasx0 <lucas.almeidasilva49@gmail.com>
2022-12-04 20:17:03 -0300

    pwa 2 commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+
 import './index.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -17,5 +18,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 serviceWorkerRegistration.register();
+
 reportWebVitals();
 
