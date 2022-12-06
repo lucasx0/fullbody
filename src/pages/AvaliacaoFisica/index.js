1064dbf26506eb9fcd3eb460a08e76321dbfa9ad
bielmorato <bielmorato09@gmail.com>
2022-12-06 18:40:29 -0300

    alterações da pagina de avaliação fisica

--- src/pages/AvaliacaoFisica/index.js
+++ src/pages/AvaliacaoFisica/index.js
@@ -1,17 +1,58 @@
 import Header from "../../components/Header"
 import Title from "../../components/Title"
+import ListaSuspensa from "../../components/listaSuspensa";
 import { FiPlusCircle } from "react-icons/fi";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 
+import api from "../../services/api";
+import { Link, useHistory } from 'react-router-dom';
 import { AuthContext } from "../../contexts/auth";
 import './AvaliacaoFisica.css';
 
 export default function AvaliacaoFisica(){
 
+  const [customer, setCustomer] = useState([]);
+  const [nameInstructor, setNameInstructor] = useState('');
   const { user, signOut } = useContext(AuthContext);
-  const [ nome, SetNome] = useState(user);
+  const [ nome, SetNome] = useState(null);
   const [ dateAvaliacao, SetDateAvaliacao] = useState(user);
   const [ instrutor, SetInstrutor] = useState(user);
+
+  const history = useHistory();
+  const id = localStorage.getItem("SistemaUser");
+  let obj = JSON.parse(id);
+
+  const token = obj.jwt;
+  const instructorID = obj.id
+
+  function handleAdd(e){
+    e.preventDefault();
+    alert('TESTE')
+  }
+
+  useEffect(() => {
+    api
+      .get("/customer/instructor/" + instructorID , {
+        headers: {
+          Authorization: "Bearer " + token
+        }
+      })
+      .then(res => {
+        setCustomer(res.data);
+        console.log(res.data);
+      });
+    
+    api
+      .get("/instructor/" + instructorID , {
+        headers: {
+          Authorization: "Bearer " + token
+        }
+      })
+      .then(res => {
+        setNameInstructor(res.data.name);
+      });
+  }, [instructorID]);
+
 
   return(
     <div>
@@ -26,107 +67,38 @@ export default function AvaliacaoFisica(){
         <div className="container">  
         <div className="nav-left">
         <form className="form-pesquisar-alunos">
-        <label>Aluno</label>   
-        <input type="text" value={user} disabled={false}/>
-        <label>Data</label>   
-        <input type="text" value={user} disabled={false}/>
-        <label>Professor</label>   
-        <input type="text" value={user} disabled={false}/>
+          <div className="card">
+          <label>Aluno</label>
+        </div>
+        <input type="text" value={customer.nome} disabled={false}/>
+        <br/>
+        <div className="card">
+        <br/>
+        <label>Agendar avaliação</label>
+        </div>
+        <input type="text" placeholder=" 00/00/0000" texvalue={nome} disabled={false}/>
+        <br/>
           <button type="submit">Cadastrar</button>
         </form>
       </div>
     </div>
-
-
-        {/*Perguntas avaliação fisica */}
     <div className="container">
-      <form className="form-profile">
-        <div>
-          <h4>Par-Q</h4>
-        </div>
-        
+    <div className="nav-left">
       <div className="card">
-        <p>Seu médico já disse que você possui um problema cardíaco e recomendou atividades físicas apenas sob supervisão médica? </p>
-        <div class="radio">
-        <input type="radio" name="1" id="1" value="S" required=""/>
-          <label for="1">Sim</label>
-         <input type="radio" name="1" id="2" value="N"/>
-         <label for="2" class="ml-2">Não</label>
-         <label for="1" class="error"></label>
-        </div>
-       </div>
-
-       <div className="card">
-        <p>Você tem dor no peito provocada por atividades físicas? * </p>
-        <div class="radio">
-        <input type="radio" name="1" id="1" value="S" required=""/>
-          <label for="1">Sim</label>
-         <input type="radio" name="1" id="2" value="N"/>
-         <label for="2" class="ml-2">Não</label>
-         <label for="1" class="error"></label>
-        </div>
-       </div>
-
-       <div className="card">
-        <p>Você sentiu dor no peito no último mês? </p>
-        <div class="radio">
-        <input type="radio" name="1" id="1" value="S" required=""/>
-          <label for="1">Sim</label>
-         <input type="radio" name="1" id="2" value="N"/>
-         <label for="2" class="ml-2">Não</label>
-         <label for="1" class="error"></label>
-        </div>
-       </div>
-
-       <div className="card">
-        <p>Você já perdeu a consciência em alguma ocasião ou sofreu alguma queda em virtude de tontura? </p>
-        <div class="radio">
-        <input type="radio" name="1" id="1" value="S" required=""/>
-          <label for="1">Sim</label>
-         <input type="radio" name="1" id="2" value="N"/>
-         <label for="2" class="ml-2">Não</label>
-         <label for="1" class="error"></label>
-        </div>
-       </div>
-
-       <div className="card">
-        <p>Você tem algum problema ósseo ou articular que poderia agravar-se com a prática de atividades físicas? </p>
-        <div class="radio">
-        <input type="radio" name="1" id="1" value="S" required=""/>
-          <label for="1">Sim</label>
-         <input type="radio" name="1" id="2" value="N"/>
-         <label for="2" class="ml-2">Não</label>
-         <label for="1" class="error"></label>
-        </div>
-       </div>
-
-       <div className="card">
-        <p>Algum médico já lhe prescreveu medicamento para pressão arterial ou para o coração? </p>
-        <div class="radio">
-        <input type="radio" name="1" id="1" value="S" required=""/>
-          <label for="1">Sim</label>
-         <input type="radio" name="1" id="2" value="N"/>
-         <label for="2" class="ml-2">Não</label>
-         <label for="1" class="error"></label>
-        </div>
-       </div>
-
-       <div className="card">
-        <p>Você tem conhecimento, por informação médica ou pela própria experiência, de algum motivo que poderia impedí-lo de participar de atividades fisicas sem supervisão médica?  </p>
-        <div class="radio">
-        <input type="radio" name="1" id="1" value="S" required=""/>
-          <label for="1">Sim</label>
-         <input type="radio" name="1" id="2" value="N"/>
-         <label for="2" class="ml-2">Não</label>
-         <label for="1" class="error"></label>
-        </div>
-       </div>
-
-      </form>
+        <p>Avaliações</p>
+      <br/>
+      <br/>
+    <div className="card">
+      {customer.length === 0 ?(
+        <span>nenhuma avaliação registrada</span>
+      ) : (
+        customer.name
+      )}
+      <br/>
     </div>
-
-    
-
+      </div>
+    </div>
+    </div>
  </div>
 </div>
 
